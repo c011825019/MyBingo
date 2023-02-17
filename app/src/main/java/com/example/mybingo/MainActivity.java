8069c4b586b5e817cd9c8a9e3101f825d5df3321
c011825019 <c011825019@gmail.com>
2023-02-17 14:27:27 +0900

    Finish Chapter5-2

--- app/src/main/java/com/example/mybingo/MainActivity.java
+++ app/src/main/java/com/example/mybingo/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.mybingo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int maxNumber = 75;
+    private EditText maxNumberEditText;
+    private Button registerMaxNumberButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        maxNumberEditText = findViewById(R.id.max_number);
+        registerMaxNumberButton = findViewById(R.id.register_max_number);
+
+        maxNumberEditText.setText("" + maxNumber);
+
+        registerMaxNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String maxNumberString = maxNumberEditText.getText().toString();
+                maxNumber = Integer.valueOf(maxNumberString);
+
+                Log.d("MainActivity", "maxNumber: " + maxNumber);
+            }
+        });
     }
 }
