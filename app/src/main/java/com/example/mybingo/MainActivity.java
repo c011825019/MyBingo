faf3e5d59425f48bf46c5e06ca3d02c3e734fd96
c011825019 <c011825019@gmail.com>
2023-02-17 14:57:38 +0900

    Finish Chapter5-3

--- app/src/main/java/com/example/mybingo/MainActivity.java
+++ app/src/main/java/com/example/mybingo/MainActivity.java
@@ -7,12 +7,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private int maxNumber = 75;
     private EditText maxNumberEditText;
     private Button registerMaxNumberButton;
+    private Button nextNumberButton;
+    private TextView currentNumberTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
         maxNumberEditText = findViewById(R.id.max_number);
         registerMaxNumberButton = findViewById(R.id.register_max_number);
+        nextNumberButton = findViewById(R.id.next_number);
+        currentNumberTextView = findViewById(R.id.current_number);
 
         maxNumberEditText.setText("" + maxNumber);
 
@@ -30,8 +35,22 @@ public class MainActivity extends AppCompatActivity {
                 String maxNumberString = maxNumberEditText.getText().toString();
                 maxNumber = Integer.valueOf(maxNumberString);
 
-                Log.d("MainActivity", "maxNumber: " + maxNumber);
+                ///Log.d("MainActivity", "maxNumber: " + maxNumber);
             }
         });
+
+        nextNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickNextNumber();
+            }
+        });
+    }
+
+    private void onClickNextNumber() {
+        double randomNumber = Math.random() * (maxNumber - 1);
+        int nextNumber = (int) randomNumber + 1;
+        currentNumberTextView.setText("" + nextNumber);
+        //Log.d("MainActivity", "nextNumber: " + nextNumber);
     }
 }
